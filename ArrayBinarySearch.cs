2c2cd107af19a851486fd4a62414249b6db1b78e
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-01-14 00:29:28 +0100

    Add project files.

---
+++ ArrayBinarySearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDemo2021
+{
+    public class ArrayBinarySearch
+    {
+        int lowIndex, highIndex, midIndex;
+
+        public int getArrayMidPoint(int [] refArray)
+        {
+            lowIndex = 0;
+            highIndex = refArray.Length - 1;
+            midIndex = (lowIndex + highIndex) / 2;
+            return midIndex;
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlgorithmDemo2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] testArray = new int[] { 0,1,2,3,4,5,6,7,8,9 };
+            ArrayBinarySearch search = new ArrayBinarySearch();
+            int arrayMidPoint = search.getArrayMidPoint(testArray);
+            Console.WriteLine($"midpoint - { arrayMidPoint}");
+            Console.ReadKey();
+        }
+    }
+}
