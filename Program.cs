de9039af43e400803ada6f79c940b0caa2c22fbf
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-01-14 01:48:36 +0100

    generated test sequence for the binary search algorithim

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AlgorithmDemo2021.BinarySearch;
+using System;
+using System.Linq;
 
 namespace AlgorithmDemo2021
 {
@@ -7,10 +9,27 @@ namespace AlgorithmDemo2021
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] testArray = new int[] { 0,1,2,3,4,5,6,7,8,9 };
-            ArrayBinarySearch search = new ArrayBinarySearch();
-            int arrayMidPoint = search.getArrayMidPoint(testArray);
-            Console.WriteLine($"midpoint - { arrayMidPoint}");
+            int[] searchItems = new int[] { 45,12,98,67,288, 7 };
+
+            int[] sequenceArray = Enumerable.Range(1,100).ToArray();
+
+            int serchItemCount = searchItems.Length - 1;
+            for(int x =0; x <= serchItemCount; x++)
+            {
+                BinarySearchAlg search = new BinarySearchAlg(sequenceArray);
+                bool isFound = search.geSearchResult(searchItems[x]).Equals(1);
+                if (isFound)
+                {
+                    Console.WriteLine($"found item - {searchItems[x]}");
+                }
+                else
+                {
+                    Console.WriteLine($"did not find item - {searchItems[x]}");
+                }
+
+            }
+            
+            
             Console.ReadKey();
         }
     }
