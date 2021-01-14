04b2516c9588170add79e9b505368b95c40a4ca2
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-01-14 01:23:06 +0100

    added generic interface to implement type for binary search

---
+++ BinarySearch/BinarySearchAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDemo2021.BinarySearch
+{
+    public class BinarySearchAlg : IBinarySearch<int>
+    {
+        private int[] searchList;
+        private int low, high, mid;
+        public BinarySearchAlg(int [] sortedListItems)
+        {
+            //assumes the array is sorted but we can try  sort it to be sure
+            Array.Sort(sortedListItems);
+            searchList = sortedListItems;
+            low = 0;
+            high = sortedListItems.Length - 1;
+        }
+
+        public int geSearchResult(int itemToFind)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BinarySearch/IBinarySearch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDemo2021.BinarySearch
+{
+    public interface IBinarySearch<T>
+    {
+        T geSearchResult(T itemToFind);
+    }
+}
